3710636d82c9f621932deb5e17e83dc1332f4ff8
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:16:50 +0430

    create product model in area

---
+++ Areas/Admin/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+    }
+}
