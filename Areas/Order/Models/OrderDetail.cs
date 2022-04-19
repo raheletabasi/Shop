4ff3bb451e16ec0f5ffc2f37eff329de0460f843
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:34:05 +0430

    create orderDetail in order area

---
+++ Areas/Order/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Areas.Admin.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Order.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Cost { get; set; }
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
