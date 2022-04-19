c197bae03481214792e9ebd67ca66cabcc39c5b9
Rahele1991 <raheleh.tabasi@gmail.com>
2022-04-19 11:05:38 +0430

    create user model in admin area

---
+++ Areas/Admin/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
