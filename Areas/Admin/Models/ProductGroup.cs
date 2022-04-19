4bc532b1009c67230e5c56eee2c1a6d5163f734c
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 13:05:05 +0430

    create productGroup model in admin area

---
+++ Areas/Admin/Models/ProductGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class ProductGroup
+    {
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        [ForeignKey("Group")]
+        public int GroupId { get; set; }
+
+        public List<Product> Product { get; set; }
+        public List<Group> Groups { get; set; }
+
+    }
+}
