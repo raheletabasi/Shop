100edae2aa8397bd681ceb293e3def0ff68c24f8
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:59:24 +0430

    add navigation property item in product model

--- Areas/Admin/Models/Product.cs
+++ Areas/Admin/Models/Product.cs
@@ -8,5 +8,7 @@ namespace Shop.Areas.Admin.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
+
+        public Item Item { get; set; }
     }
 }
