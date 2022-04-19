d092ccb5bc297836f30f28fa1822fd2cd455014e
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:29:56 +0430

    create order model in order area

---
+++ Areas/Order/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Areas.Admin.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Order.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalCost { get; set; }
+        public bool IsClose { get; set; }
+
+        public User User { get; set; }
+    }
+}
