e1f435d5d26b7a43bb9f312c24ae03a306001c7e
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:18:46 +0430

    create group model in area admin

---
+++ Areas/Admin/Models/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class Group
+    {
+        [Key]
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
