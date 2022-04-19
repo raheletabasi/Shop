bd96ed539459007aad482a3e12e9655703b57b08
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:23:59 +0430

    create item model in area admin

---
+++ Areas/Admin/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public int ProductId { get; set; }
+        public int QuntityInStock { get; set; }
+        public decimal Cost { get; set; }
+
+        public Product product { get; set; }
+    }
+}
