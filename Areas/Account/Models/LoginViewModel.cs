a074c2411bfb00e67f2186106e098d2f00916889
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 11:45:38 +0430

    create loginViewModel in account area

---
+++ Areas/Account/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Areas.Account.Models
+{
+    public class LoginViewModel
+    {       
+        [Required(ErrorMessage = "لطفا ایمیل را وارد نمایید!")]
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "لطفا پسورد را وارد نمایید!")]
+        [Display(Name = "رمز عبور")]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
