18998f63715463f3fcebad287499c63600e75611
Rahele <raheleh.tabasi@gmail.com>
2022-04-19 18:25:56 +0430

    add login Controller for get and post

---
+++ Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Areas.Account.Models;
+using Shop.Data;
+using System.Linq;
+
+namespace Shop.Areas.Account.Controllers
+{
+    [Area("Account")]
+    public class AccountController : Controller
+    {
+        readonly ShopDbContext _dbContext;
+
+        public AccountController(ShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            var userLogin = _dbContext.Users
+                .Where(usr => usr.Email == loginViewModel.Email.ToLower()
+                           && usr.IsActive == true);
+
+            if (userLogin == null)
+            {
+                ModelState.AddModelError(string.Empty, $"ایمیل {0} در سامانه ثبت نشده است");
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Register", "Account");
+        }
+
+        
+    }
+}
