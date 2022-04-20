6026291073315c36180cc222900058e5fad34d4f
Rahele <raheleh.tabasi@gmail.com>
2022-04-20 15:19:50 +0430

    add share view and default model to area

---
+++ Areas/Account/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop.Areas.Account.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Areas/Account/Models/LoginViewModel.cs
+++ Areas/Account/Models/LoginViewModel.cs
@@ -1,15 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Areas.Account.Models
 {
     public class LoginViewModel
     {       
-        [Required(ErrorMessage = "لطفا ایمیل را وارد نمایید!")]
+        [Required(ErrorMessage = "لطفا ایمیل را وارد نمایید!")]       
         [Display(Name = "ایمیل")]
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "لطفا پسورد را وارد نمایید!")]
         [Display(Name = "رمز عبور")]
+        [Remote("","")]
         public string Password { get; set; }
 
         [Display(Name = "مرا به خاطر بسپار")]
